479b5ac16ad2f69da91ce402722d74d282411107
Alan Almeida <alan241015@gmail.com>
2021-08-19 14:59:30 +0530

    add models

--- getweather/__init__.py
+++ getweather/__init__.py
@@ -1,7 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../instance/main.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
 from getweather import views, models

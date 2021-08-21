6023117828f845438daadb33952bcd2a1571d539
Alan Almeida <alan241015@gmail.com>
2021-08-21 18:51:54 +0530

    add display logic

--- getweather/__init__.py
+++ getweather/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 app = Flask(__name__)
 
@@ -9,10 +13,9 @@ try:
 except OSError:
     pass
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../instance/main.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
-
 
 from getweather import views, models
--- getweather/views.py
+++ getweather/views.py
@@ -3,13 +3,16 @@ from flask import render_template, request, url_for, flash
 from werkzeug.utils import redirect
 from getweather import app, db
 from getweather.models import City
+import os
+
+API_KEY = os.environ.get("API_KEY")
 
 @app.route('/', method=["GET", "POST"])
 @app.route('/index', method=["GET", "POST"])
 def index():
     if request.method == "POST":
-        city = request.form('city')
-        data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=dc34755a51fe4175feca0b7eca4b7219").json()
+        city = request.form('city').strip()
+        data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}").json()
         try:
             if City.query.filter_by(city=city).first() is not None:
                 flash("City Already Exists")
@@ -26,4 +29,14 @@ def index():
         except:
             flash("Something Went Wrong!")
             return redirect(url_for("index"))
-    return render_template(url_for('index'))
+    
+    cities = []
+    for city in City.query.all():
+        city_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city.name}&appid={API_KEY}")
+        city_obj = {
+            "name": city_data["name"],
+            "desc": ", ".join(i["description"] for i in city_data["weather"]),
+            "temp": city_data["main"]["temp"]
+        }
+        cities.append(city_obj)
+    return render_template(url_for('index'), cities=cities)

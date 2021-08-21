e6ac8e68cf1960b117be14c5e9e2b670da05bda6
Alan Almeida <alan241015@gmail.com>
2021-08-21 17:47:14 +0530

    added post logic

--- getweather/views.py
+++ getweather/views.py
@@ -1,9 +1,29 @@
 import requests
-from flask import render_template, request
-from getweather import app
+from flask import render_template, request, url_for, flash
+from werkzeug.utils import redirect
+from getweather import app, db
+from getweather.models import City
 
 @app.route('/', method=["GET", "POST"])
+@app.route('/index', method=["GET", "POST"])
 def index():
     if request.method == "POST":
-        data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={request.form('city')}&appid=dc34755a51fe4175feca0b7eca4b7219").json()
-    return render_template('index.html')
+        city = request.form('city')
+        data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=dc34755a51fe4175feca0b7eca4b7219").json()
+        try:
+            if City.query.filter_by(city=city).first() is not None:
+                flash("City Already Exists")
+                return redirect(url_for('index'))
+            elif data['cod'] == 200:
+                c = City(city=city)
+                db.session.add(c)
+                db.session.commit()
+                flash(f"{city} Added!")
+                return redirect(url_for('index'))
+            else:
+                flash("Entered city not found! Please enter a valid city!")
+                return redirect(url_for("index"))
+        except:
+            flash("Something Went Wrong!")
+            return redirect(url_for("index"))
+    return render_template(url_for('index'))
